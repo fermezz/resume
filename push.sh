1e4903b584a95875d01d6627e3e1cd865e73d5df
Fernando Mez <fermezz@gmail.com>
2020-01-03 21:09:29 -0300

    Always push to a different branch. Improve commit message.

--- push.sh
+++ push.sh
@@ -8,12 +8,12 @@ setup_git() {
 commit_resume_files() {
   git checkout -b resume-changes
   git add resume*
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git commit --message "Update resume.pdf automatically."
 }
 
 upload_files() {
   git remote add origin-resume https://${GITHUB_TOKEN}@github.com/fermezz/resume.git
-  git push --quiet --set-upstream origin-resume resume-changes
+  git push --quiet --set-upstream origin-resume resume-changes-${TRAVIS_BUILD_NUMBER}
 }
 
 setup_git
